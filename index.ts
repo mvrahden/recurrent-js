35c14bfce9b73e129d4e0fd106654912aaa0f9ee
Menno <menno.vrahden@gmail.com>
2017-11-30 13:44:56 +0100

    update tsconfig, add declaration files, introduce dist directory

--- index.ts
+++
@@ -1,8 +0,0 @@
-import { LSTM } from './src/LSTM';
-import { Net } from './src/Net';
-import { Graph } from './src/Graph';
-import { RandMat } from './src/RandMat';
-import { Mat } from './src/Mat';
-import { R } from './src/R';
-
-export { R, Mat, RandMat, Graph, Net, LSTM };
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import { LSTM } from './LSTM';
+import { Net } from './Net';
+import { Graph } from './Graph';
+import { RandMat } from './RandMat';
+import { Mat } from './Mat';
+import { R } from './R';
+
+export { R, Mat, RandMat, Graph, Net, LSTM };
