cf600fd6180a56857b21e90b6f10ca25ac6a5a64
Menno <menno.vrahden@gmail.com>
2018-04-19 09:42:29 +0200

    refactoring

--- src/Net.ts
+++ src/Net.ts
@@ -3,38 +3,59 @@ import { RandMat } from './RandMat';
 import { Graph } from './Graph';
 
 export class Net {
-  public readonly W1: Mat | null = null;
-  public readonly b1: Mat | null = null;
-  public readonly W2: Mat | null = null;
-  public readonly b2: Mat | null = null;
+  public W1: Mat | null = null;
+  public b1: Mat | null = null;
+  public W2: Mat | null = null;
+  public b2: Mat | null = null;
 
   /**
-   * Generates a `null`-initialized Neural Net.
+   * Generates a default initialized Neural Net.
+   * Default Setup:
+   * @param {{inputSize: number = 1, hiddenUnits: number = 1, outputSize: number = 1}} opt Specs of the Neural Net.
    */
   constructor();
   /**
    * Generates a Neural Net instance from a pretrained Neural Net JSON.
    * @param {{W1, b1, W2, b2}} opt Specs of the Neural Net.
    */
-  constructor(opt: any);
+  constructor(opt: { W1, b1, W2, b2 });
   /**
    * Generates a Neural Net with given specs.
    * @param {{inputSize: number, hiddenSize: number, outputSize: number, mu: number = 0, std: number = 0.01}} opt Specs of the Neural Net.
    */
-  constructor(opt?: {inputSize: number, hiddenSize: number, outputSize: number, mu?: number, std?: number}) {
-    if (Net.has(opt, ['W1', 'b1', 'W2', 'b2'])) {
-      this.W1 = Mat.fromJSON(opt['W1']);
-      this.b1 = Mat.fromJSON(opt['b1']);
-      this.W2 = Mat.fromJSON(opt['W2']);
-      this.b2 = Mat.fromJSON(opt['b2']);
-    } else if (Net.has(opt, ['inputSize', 'hiddenSize', 'outputSize'])) {
-      const mu = opt['mu'] ? opt['mu'] : 0;
-      const std = opt['std'] ? opt['std'] : 0.01;
-      this.W1 = new RandMat(opt['hiddenSize'], opt['inputSize'], mu, std);
-      this.b1 = new Mat(opt['hiddenSize'], 1);
-      this.W2 = new RandMat(opt['outputSize'], opt['hiddenSize'], mu, std);
-      this.b2 = new Mat(opt['outputSize'], 1);
+  constructor(opt: { inputSize: number, hiddenUnits: number, outputSize: number, mu?: number, std?: number });
+  constructor(opt?: any) {
+    if (this.isFromJSON(opt)) {
+      this.initializeFromJSONObject(opt);
+    } else if (this.isFreshInstanceCall(opt)) {
+      this.initializeAsFreshInstance(opt);
+    } else {
+      this.initializeAsFreshInstance({inputSize: 1, hiddenUnits: 1, outputSize: 1 });
     }
+  }
+
+  private isFromJSON(opt: any) {
+    return Net.has(opt, ['W1', 'b1', 'W2', 'b2']);
+  }
+
+  private isFreshInstanceCall(opt: { inputSize: number; hiddenUnits: number; outputSize: number; mu?: number; std?: number; }) {
+    return Net.has(opt, ['inputSize', 'hiddenUnits', 'outputSize']);
+  }
+
+  private initializeFromJSONObject(opt: { W1, b1, W2, b2 }) {
+    this.W1 = Mat.fromJSON(opt['W1']);
+    this.b1 = Mat.fromJSON(opt['b1']);
+    this.W2 = Mat.fromJSON(opt['W2']);
+    this.b2 = Mat.fromJSON(opt['b2']);
+  }
+
+  private initializeAsFreshInstance(opt: { inputSize: number; hiddenUnits: number; outputSize: number; mu?: number; std?: number; }) {
+    const mu = opt['mu'] ? opt['mu'] : 0;
+    const std = opt['std'] ? opt['std'] : 0.01;
+    this.W1 = new RandMat(opt['hiddenUnits'], opt['inputSize'], mu, std);
+    this.b1 = new Mat(opt['hiddenUnits'], 1);
+    this.W2 = new RandMat(opt['outputSize'], opt['hiddenUnits'], mu, std);
+    this.b2 = new Mat(opt['outputSize'], 1);
   }
 
   /**
@@ -59,22 +80,28 @@ export class Net {
 
   /**
    * Forward propagation for a single tick of RNN
-   * @param observations 1D column vector with observations
+   * @param state 1D column vector with observations
    * @param graph optional: inject Graph to append Operations
    * @returns Structure containing hidden representation ['h'] of type `Mat[]` and output ['output'] of type `Mat`
    */
-  public forward(observations: Mat, graph: Graph): Mat {
-    // TODO: Is this a Deep Net? Hyperbolic activation h1 is a Vector of size hiddenUnits (= each Neuron is one Layer???)
-    const weightedStates = graph.mul(this.W1, observations);
+  public forward(state: Mat, graph: Graph): Mat {
+    // TODO: Is this a Deep Net? 
+    // TODO: Hyperbolic activation h1 is a Vector of size hiddenUnits
+    const weightedInput = graph.mul(this.W1, state);
     // a1 = Activation Input Vector of Weights 1 (W1) and stateVector
-    const a1mat = graph.add(weightedStates, this.b1);
+    const a1mat = graph.add(weightedInput, this.b1);
+
     // h1 = Hyperbolic activation
     const h1mat = graph.tanh(a1mat);
 
-    const weightedActivations = graph.mul(this.W2, h1mat);
-    // a2 = Action Vector of Weight2 (W2) and hyperbolic Activation (h1)
-    const a2Mat = graph.add(weightedActivations, this.b2);
-    // TODO: Hyperbolic activation of a2Mat
+    const a2Mat = this.computeOutput(h1mat, graph);
+    return a2Mat;
+  }
+
+  private computeOutput(hiddenUnits: Mat, graph: Graph) {
+    const weightedActivation = graph.mul(this.W2, hiddenUnits);
+    // a2 = Output Vector of Weight2 (W2) and hyperbolic Activation (h1)
+    const a2Mat = graph.add(weightedActivation, this.b2);
     return a2Mat;
   }
 
