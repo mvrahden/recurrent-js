b18af72115d5a2cc77a3e0fc79af62eaf052f7fc
Menno van Rahden <menno.vrahden@gmail.com>
2018-05-03 11:03:35 +0100

    add first tests

--- src/mat.spec.ts
+++ src/mat.spec.ts
@@ -1,25 +1,26 @@
-// import { Mat } from '../../dist/index';
+import { Mat } from '.';
 
-// describe('Mat:', () => {
-//   it('should multiply matrices', () => {
-//     // initialize
-//     const mat1 = new Mat(2, 4);
-//     mat1.setFrom([1, 4, 6, 10, 2, 7, 5, 3]);
-//     const mat2 = new Mat(4, 3);
-//     mat2.setFrom([1, 4, 6, 2, 7, 5, 9, 0, 11, 3, 1, 0]);
+describe('Mat:', () => {
+  it('should multiply matrices', () => {
+    // initialize
+    const mat1 = new Mat(2, 4);
+    mat1.setFrom([1, 4, 6, 10, 2, 7, 5, 3]);
+    const mat2 = new Mat(4, 3);
+    mat2.setFrom([1, 4, 6, 2, 7, 5, 9, 0, 11, 3, 1, 0]);
 
-//     // create actual output
-//     const actual = Mat.mul(mat1, mat2);
+    // create actual output
+    const actual = Mat.mul(mat1, mat2);
 
-//     // create expected output
-//     const expected = new Mat(2, 3);
-//     expected.setFrom([93, 42, 92, 70, 60, 102]);
+    // create expected output
+    const expected = new Mat(2, 3);
+    expected.setFrom([93, 42, 92, 70, 60, 102]);
 
-//     // test
-//     actual.w.forEach((value, i) => {
-//       expect(value).toBe(expected.w[i]);
-//     });
-//     expect(actual.rows).toBe(expected.rows);
-//     expect(actual.cols).toBe(expected.cols);
-//   });
-// });
+    // test dimension
+    expect(actual.rows).toBe(expected.rows);
+    expect(actual.cols).toBe(expected.cols);
+    // test content
+    for(let i=0; i < actual.w.length; i++) {
+      expect(actual.w[i]).toBe(expected.w[i]);
+    }
+  });
+});
