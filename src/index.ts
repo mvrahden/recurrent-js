00ae33c63d8ac9bd59d60c29e1bc9090395c09db
Menno van Rahden <menno.vrahden@gmail.com>
2018-05-03 11:02:40 +0100

    fix imports and exports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import { BNN } from './fnn/bnn';
 import { DNN } from './fnn/dnn';
-import { FNNModel } from './fnn/fnn-model';
 import { Graph } from './graph';
 import { InnerState } from './utils/inner-state';
 import { LSTM } from './rnn/lstm';
@@ -10,7 +9,5 @@ import { NetOpts } from './utils/net-opts';
 import { Utils } from './utils';
 import { RandMat } from './rand-mat';
 import { RNN } from './rnn/rnn';
-import { RNNModel } from './rnn/rnn-model';
-import { Solver } from './solver';
 
-export { Utils, Mat, DNN, RandMat, Graph, Net, LSTM, RNN, RNNModel, FNNModel, InnerState, NetOpts, Solver };
+export { BNN, DNN, Graph, InnerState, LSTM, Mat, Net, NetOpts, Utils, RandMat, RNN };
