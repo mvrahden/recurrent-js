09acf82df07c36e58cf9809d9535f839ec15125c
Menno van Rahden <menno.vrahden@gmail.com>
2018-05-17 14:42:53 +0100

    fix fnn-model and add tests

---
+++ src/fnn/fnn-model.spec.ts
@@ -0,0 +1,195 @@
+import { DNN } from '../.';
+
+/**
+ * Tests are being executed on a DNN instance.
+ * DNN Class fully delegates instantiation to FNNModel Class.
+ */
+describe('Feedforward Neural Network Model:', () => {
+  let sut: DNN;
+
+  describe('Instantiation:', () => {
+    
+    describe('Configuration with NetOpts:', () => {
+
+      const config = {
+        inputSize: 2, hiddenUnits: [3, 4], outputSize: 3
+      };
+
+      beforeEach(() => {
+        sut = new DNN(config);
+      });
+
+      it('fresh instance >> on creation >> should hold model with hidden layer, containing arrays of weight and bias matrices', () => {
+        expect(sut.model).toBeDefined();
+        expect(sut.model.hidden).toBeDefined();
+        expect(sut.model.hidden.Wh).toBeDefined();
+        expect(sut.model.hidden.Wh.length).toBe(2);
+        expect(sut.model.hidden.bh).toBeDefined();
+        expect(sut.model.hidden.bh.length).toBe(2);
+      });
+
+      it('fresh instance >> on creation >> should hold model with decoder layer, containing weight and bias matrices', () => {
+        expect(sut.model.decoder).toBeDefined();
+        expect(sut.model.decoder.Wh).toBeDefined();
+        expect(sut.model.decoder.b).toBeDefined();
+      });
+    
+      describe('Hidden Layer:', () => {
+
+        it('fresh instance >> on creation >> model should hold hidden layer containing weight matrices with expected dimensions', () => {
+          expectHiddenWeightMatricesToHaveColsOfSizeOfPreviousLayerAndRowsOfConfiguredLength(2, [3, 4]);
+        });
+
+        it('fresh instance >> on creation >> model should hold hidden layer containing bias matrices with expected dimensions', () => {
+          expectHiddenBiasMatricesToHaveRowsOfSizeOfPreviousLayerAndColsOfSize1(2, [3, 4]);
+        });
+
+        const expectHiddenWeightMatricesToHaveColsOfSizeOfPreviousLayerAndRowsOfConfiguredLength = (inputSize: number, hiddenUnits: Array<number>) => {
+          let previousLayerSize = inputSize;
+          let expectedRows, expectedCols;
+          for (let i = 0; i < sut.model.hidden.Wh.length; i++) {
+            expectedRows = hiddenUnits[i];
+            expectedCols = previousLayerSize;
+            expect(sut.model.hidden.Wh[i].rows).toBe(expectedRows);
+            expect(sut.model.hidden.Wh[i].cols).toBe(expectedCols);
+            previousLayerSize = expectedRows;
+          }
+        };
+
+        const expectHiddenBiasMatricesToHaveRowsOfSizeOfPreviousLayerAndColsOfSize1 = (inputSize: number, hiddenUnits: Array<number>) => {
+          let previousLayerSize = inputSize;
+          let expectedRows;
+          let expectedCols = 1;
+          for (let i = 0; i < sut.model.hidden.Wh.length; i++) {
+            expectedRows = hiddenUnits[i];
+            expect(sut.model.hidden.bh[i].rows).toBe(expectedRows);
+            expect(sut.model.hidden.bh[i].cols).toBe(expectedCols);
+            previousLayerSize = expectedRows;
+          }
+        };
+      });
+    
+      describe('Decoder Layer:', () => {
+
+        it('fresh instance >> on creation >> model should hold decoder layer containing matrices with expected dimensions', () => {
+          sut = new DNN(config);
+
+          expectDecoderWeightMatrixToHaveDimensionsOf(3, 4);
+        });
+
+        it('fresh instance >> on creation >> model should hold decoder layer containing matrices with expected dimensions', () => {
+          sut = new DNN(config);
+
+          expectDecoderBiasMatrixToHaveDimensionsOf(3, 1);
+        });
+  
+        const expectDecoderWeightMatrixToHaveDimensionsOf = (expectedRows: number, expectedCols: number) => {
+          expect(sut.model.decoder.Wh.rows).toBe(expectedRows);
+          expect(sut.model.decoder.Wh.cols).toBe(expectedCols);
+        };
+  
+        const expectDecoderBiasMatrixToHaveDimensionsOf = (expectedRows: number, expectedCols: number) => {
+          expect(sut.model.decoder.b.rows).toBe(expectedRows);
+          expect(sut.model.decoder.b.cols).toBe(expectedCols);
+        };
+      });
+    });
+
+    describe('Configuration with JSON Object', () => {
+      
+    });
+  });
+  
+  describe('Backpropagation:', () => {
+  
+    beforeEach(() => {
+      const config = { inputSize: 2, hiddenUnits: [3, 4], outputSize: 3 };
+      sut = new DNN(config);
+  
+      spyOnUpdateMethods();
+    });
+  
+    describe('Backward:', () => {
+  
+      describe('Hidden Layer:', () => {
+
+        it('after forward pass >> backward >> should call graph to execute', () => {
+
+        })
+        
+      });
+  
+      describe('Decoder Layer:', () => {
+  
+      });
+    });
+  
+    describe('Update:', () => {
+  
+      describe('Hidden Layer:', () => {
+      
+        it('fresh instance >> update >> should call update methods of weight and bias matrices of all hidden layer', () => {
+          sut.update(0.01);
+  
+          expectUpdateOfLayersMethodsToHaveBeenCalled();
+        });
+  
+        it('fresh instance >> update >> should call update methods of weight and bias matrices of all hidden layer with given value', () => {
+          sut.update(0.01);
+  
+          expectUpdateOfLayersMethodsToHaveBeenCalledWithValue(0.01);
+        });
+  
+        const expectUpdateOfLayersMethodsToHaveBeenCalled = () => {
+          expect(sut.model.hidden.Wh[0].update).toHaveBeenCalled();
+          expect(sut.model.hidden.Wh[1].update).toHaveBeenCalled();
+          expect(sut.model.hidden.bh[0].update).toHaveBeenCalled();
+          expect(sut.model.hidden.bh[1].update).toHaveBeenCalled();
+        };
+  
+        const expectUpdateOfLayersMethodsToHaveBeenCalledWithValue = (value: number) => {
+          expect(sut.model.hidden.Wh[0].update).toHaveBeenCalledWith(value);
+          expect(sut.model.hidden.Wh[1].update).toHaveBeenCalledWith(value);
+          expect(sut.model.hidden.bh[0].update).toHaveBeenCalledWith(value);
+          expect(sut.model.hidden.bh[1].update).toHaveBeenCalledWith(value);
+        };
+      });
+  
+      describe('Decoder Layer:', () => {
+      
+        it('fresh instance >> update >> should call update methods of weight and bias matrices of decoder layer', () => {
+          sut.update(0.01);
+          
+          expectUpdateOfLayersMethodsToHaveBeenCalled();
+        });
+        
+        it('fresh instance >> update >> should call update methods of weight and bias matrices of decoder layer with given value', () => {
+          sut.update(0.01);
+          
+          expectUpdateOfLayersMethodsToHaveBeenCalledWithValue(0.01);
+        });
+  
+        const expectUpdateOfLayersMethodsToHaveBeenCalled = () => {
+          expect(sut.model.decoder.Wh.update).toHaveBeenCalled();
+          expect(sut.model.decoder.b.update).toHaveBeenCalled();
+        };
+  
+        const expectUpdateOfLayersMethodsToHaveBeenCalledWithValue = (value: number) => {
+          expect(sut.model.decoder.Wh.update).toHaveBeenCalledWith(value);
+          expect(sut.model.decoder.b.update).toHaveBeenCalledWith(value);
+        };
+      });
+    });
+  
+    const spyOnUpdateMethods = () => {
+      spyOn(sut.model.hidden.Wh[0], 'update');
+      spyOn(sut.model.hidden.Wh[1], 'update');
+      spyOn(sut.model.hidden.bh[0], 'update');
+      spyOn(sut.model.hidden.bh[1], 'update');
+  
+      spyOn(sut.model.decoder.Wh, 'update');
+      spyOn(sut.model.decoder.b, 'update');
+    };
+  });
+});
+
--- src/fnn/fnn-model.ts
+++ src/fnn/fnn-model.ts
@@ -2,37 +2,42 @@ import { Graph, Mat, RandMat, NetOpts } from './..';
 import { Assertable } from './../utils/assertable';
 
 export abstract class FNNModel extends Assertable {
-  protected inputSize: number;
-  protected hiddenUnits: Array<number>;
-  protected outputSize: number;
+  public inputSize: number;
+  public hiddenUnits: Array<number>;
+  public outputSize: number;
 
-  public readonly model: {hidden: {Wh: Mat[], bh: Mat[]}, decoder: {Wh: Mat, b: Mat}};
-  protected readonly graph: Graph;
+  public model: {hidden: {Wh: Mat[], bh: Mat[]}, decoder: {Wh: Mat, b: Mat}} = {hidden: {Wh:[], bh:[]}, decoder: {Wh: null, b: null}};
+  protected graph: Graph;
   
   /**
-   * Generates a Neural Net instance from a pretrained Neural Net JSON.
+   * Generates a Neural Net instance from a pre-trained Neural Net JSON.
    * @param {{ hidden: { Wh, bh }, decoder: { Wh: Mat, b: Mat } }} opt Specs of the Neural Net.
    */
   constructor(opt: { hidden: { Wh, bh }, decoder: { Wh: Mat, b: Mat } });
   /**
    * Generates a Neural Net with given specs.
-   * @param {{inputSize: number, hiddenSize: Array<number>, outputSize: number, needsBackprop: boolean = true, mu: number = 0, std: number = 0.01}} opt Specs of the Neural Net.
+   * @param {NetOpts} opt Specs of the Neural Net.
    */
   constructor(opt: NetOpts);
   constructor(opt: any) {
     super();
-    const needsBackprop = opt && opt.needsBackprop ? opt.needsBackprop : true;
 
-    this.model = this.initializeNetworkModel();
-    this.graph = new Graph(needsBackprop);
-
-    if (this.isFromJSON(opt)) {
+    const needsBackpropagation = opt && opt.needsBackprop ? opt.needsBackprop : true;
+    this.graph = new Graph(needsBackpropagation);
+    
+    if (FNNModel.isFromJSON(opt)) {
       this.initializeModelFromJSONObject(opt);
-    } else if (this.isFreshInstanceCall(opt)) {
+    } else if (FNNModel.isFreshInstanceCall(opt)) {
       this.initializeModelAsFreshInstance(opt);
     } else {
       FNNModel.assert(false, 'Improper input for DNN.');
     }
+  }
+
+  private static isFromJSON(opt: any) {
+    return FNNModel.has(opt, ['hidden', 'decoder'])
+      && FNNModel.has(opt.hidden, ['Wh', 'bh'])
+      && FNNModel.has(opt.decoder, ['Wh', 'b']);
   }
 
   protected initializeModelFromJSONObject(opt: { hidden: { Wh, bh }, decoder: { Wh, b } }) {
@@ -41,12 +46,58 @@ export abstract class FNNModel extends Assertable {
     this.model.decoder.b = Mat.fromJSON(opt['decoder']['b']);
   }
 
-  protected initializeHiddenLayerFromJSON(opt: { hidden: { Wh: any; bh: any; }; decoder: { Wh: any; b: any; }; }): void {
-    FNNModel.assert(!Array.isArray(opt['hidden']['Wh']), 'Wrong JSON Format to recreat Hidden Layer.');
+  protected initializeHiddenLayerFromJSON(opt: { hidden: { Wh: Mat[]; bh: Mat[]; }; decoder: { Wh: Mat; b: Mat; }; }): void {
+    FNNModel.assert(!Array.isArray(opt['hidden']['Wh']), 'Wrong JSON Format to recreate Hidden Layer.');
     for (let i = 0; i < opt.hidden.Wh.length; i++) {
       this.model.hidden.Wh[i] = Mat.fromJSON(opt.hidden.Wh[i]);
       this.model.hidden.bh[i] = Mat.fromJSON(opt.hidden.bh[i]);
     }
+  }
+
+  private static isFreshInstanceCall(opt: { inputSize: number; hiddenUnits: Array<number>; outputSize: number; mu?: number; std?: number; }) {
+    return FNNModel.has(opt, ['inputSize', 'hiddenUnits', 'outputSize']);
+  }
+
+  private initializeModelAsFreshInstance(opt: NetOpts) {
+    this.inputSize = opt.inputSize;
+    this.hiddenUnits = opt.hiddenUnits;
+    this.outputSize = opt.outputSize;
+    const mu = opt['mu'] ? opt['mu'] : 0;
+    const std = opt['std'] ? opt['std'] : 0.01;
+
+    this.model = this.initializeFreshNetworkModel();
+
+    this.initializeHiddenLayer(mu, std);
+
+    this.initializeDecoder(mu, std);
+  }
+
+  private initializeFreshNetworkModel(): { hidden: { Wh: Mat[]; bh: Mat[]; }; decoder: { Wh: Mat; b: Mat; }; } {
+    return {
+      hidden: {
+        Wh: new Array<Mat>(this.hiddenUnits.length),
+        bh: new Array<Mat>(this.hiddenUnits.length)
+      },
+      decoder: {
+        Wh: null,
+        b: null
+      }
+    };
+  }
+
+  private initializeHiddenLayer(mu: number, std: number) {
+    let hiddenSize;
+    for (let d = 0; d < this.hiddenUnits.length; d++) {
+      const previousSize = d === 0 ? this.inputSize : this.hiddenUnits[d - 1];
+      hiddenSize = this.hiddenUnits[d];
+      this.model.hidden.Wh[d] = new RandMat(hiddenSize, previousSize, mu, std);
+      this.model.hidden.bh[d] = new Mat(hiddenSize, 1);
+    }
+  }
+
+  private initializeDecoder(mu: number, std: number) {
+    this.model.decoder.Wh = new RandMat(this.outputSize, this.hiddenUnits[this.hiddenUnits.length - 1], mu, std);
+    this.model.decoder.b = new Mat(this.outputSize, 1);
   }
 
   /**
@@ -71,62 +122,12 @@ export abstract class FNNModel extends Assertable {
   }
 
   /**
-   * Forward pass for a single tick of Neural Network
+   * Compute forward pass of Neural Network
    * @param state 1D column vector with observations
    * @param graph optional: inject Graph to append Operations
    * @returns Output of type `Mat`
    */
   public abstract forward(state: Mat, graph?: Graph): Mat;
-
-  private initializeModelAsFreshInstance(opt: { inputSize: number; hiddenUnits: Array<number>; outputSize: number; mu?: number; std?: number; }) {
-    this.inputSize = opt.inputSize;
-    this.hiddenUnits = opt.hiddenUnits;
-    this.outputSize = opt.outputSize;
-    
-    const mu = opt['mu'] ? opt['mu'] : 0;
-    const std = opt['std'] ? opt['std'] : 0.01;
-
-    this.initializeHiddenLayer(mu, std);
-
-    this.initializeDecoder(mu, std);
-  }
-
-  private initializeDecoder(mu: number, std: number) {
-    this.model.decoder.Wh = new RandMat(this.outputSize, this.hiddenUnits[this.hiddenUnits.length - 1], mu, std);
-    this.model.decoder.b = new Mat(this.outputSize, 1);
-  }
-
-  private initializeHiddenLayer(mu: number, std: number) {
-    let hiddenSize;
-    for (let d = 0; d < this.hiddenUnits.length; d++) {
-      const previousSize = d === 0 ? this.inputSize : this.hiddenUnits[d - 1];
-      hiddenSize = this.hiddenUnits[d];
-      this.model.hidden.Wh[d] = new RandMat(hiddenSize, previousSize, mu, std);
-      this.model.hidden.bh[d] = new Mat(hiddenSize, 1);
-    }
-  }
-
-  private isFromJSON(opt: any) {
-    return FNNModel.has(opt, ['hidden', 'decoder'])
-      && FNNModel.has(opt.hidden, ['Wh', 'bh'])
-      && FNNModel.has(opt.decoder, ['Wh', 'b']);
-  }
-
-  private isFreshInstanceCall(opt: { inputSize: number; hiddenUnits: Array<number>; outputSize: number; mu?: number; std?: number; }) {
-    return FNNModel.has(opt, ['inputSize', 'hiddenUnits', 'outputSize']);
-  }
-
-  private initializeNetworkModel(): { hidden: { Wh: Mat[]; bh: Mat[]; }; decoder: { Wh: Mat; b: Mat; }; } {
-    return {
-      hidden: {
-        Wh: new Array<Mat>(this.hiddenUnits.length),
-        bh: new Array<Mat>(this.hiddenUnits.length)
-      },
-      decoder: {
-        Wh: null,
-        b: null
-      } };
-  }
 
   protected computeOutput(hiddenUnitActivations: Mat[], graph: Graph): Mat {
     const weightedInputs = graph.mul(this.model.decoder.Wh, hiddenUnitActivations[hiddenUnitActivations.length - 1]);
