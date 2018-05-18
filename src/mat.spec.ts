e323a415e24b9e03f7cd3c013472b49170018585
Menno van Rahden <menno.vrahden@gmail.com>
2018-05-18 02:06:26 +0100

    extend test coverage

--- src/mat.spec.ts
+++ src/mat.spec.ts
@@ -1,4 +1,4 @@
-import { Mat } from ".";
+import { Mat, Utils } from ".";
 
 
 describe('Matrix Object:', () => {
@@ -14,28 +14,28 @@ describe('Matrix Object:', () => {
       expect(sut.cols).toBe(3);
       expect(sut['_length']).toBe(6);
     });
-    
+
     it('fresh instance >> on creation >> should have array of values [w] and derivatives [dw] with given length', () => {
       expect(sut.w.length).toBe(6);
       expect(sut.dw.length).toBe(6);
     });
-    
+
     it('fresh instance >> on creation >> should have array of values [w] populated with zeros', () => {
       expectValuesToBe(0);
     });
-    
+
     it('fresh instance >> on creation >> should have array of derivatives [dw] populated with zeros', () => {
       expectDerivativesToBe(0);
     });
 
     const expectValuesToBe = (value: number) => {
-      for(let i = 0; i < sut.w.length; i++) {
+      for (let i = 0; i < sut.w.length; i++) {
         expect(sut.w[i]).toBe(value);
       }
     };
 
     const expectDerivativesToBe = (value: number) => {
-      for(let i = 0; i < sut.w.length; i++) {
+      for (let i = 0; i < sut.w.length; i++) {
         expect(sut.w[i]).toBe(value);
       }
     };
@@ -58,8 +58,8 @@ describe('Matrix Object:', () => {
     });
 
     it('with [0, 3, 2, 4, 5, 1] populated instance >> get value with given row and col >> should return value at position', () => {
-      expectValueAtGivenPosition({row: 1, col: 2}, 1);
-      expectValueAtGivenPosition({row: 0, col: 1}, 3);
+      expectValueAtGivenPosition({ row: 1, col: 2 }, 1);
+      expectValueAtGivenPosition({ row: 0, col: 1 }, 3);
     });
 
     it('with [0, 3, 2, 4, 5, 1] populated instance >> set value at given row and col >> should mutate value at given position', () => {
@@ -90,26 +90,134 @@ describe('Matrix Object:', () => {
     });
 
     const expectColumnToContain = (col: number, expected: Array<number>) => {
-      for(let i = 0; i < expected.length; i++) {
+      for (let i = 0; i < expected.length; i++) {
         expect(sut.get(i, col)).toBe(expected[i]);
       }
     };
-    
+
     const expectValueAtGivenPosition = (given: any, expected: number) => {
       let actual = sut.get(given.row, given.col);
       expect(actual).toBe(expected);
     };
 
     const expectValuesToBe = (expected: Array<number>) => {
-      for(let i = 0; i < expected.length; i++) {
+      for (let i = 0; i < expected.length; i++) {
         expect(sut.w[i]).toBe(expected[i]);
       }
     };
 
     const expectDerivativesToBe = (expected: Array<number>) => {
-      for(let i = 0; i < expected.length; i++) {
+      for (let i = 0; i < expected.length; i++) {
         expect(sut.dw[i]).toBe(expected[i]);
       }
     };
+  });
+
+  describe('Backpropagation:', () => {
+    let sut: Mat;
+
+    beforeEach(() => {
+      sut = new Mat(2, 3);
+      sut.setFrom([0, 1, 2, 3, 4, 5]);
+      Utils.fillConst(sut.dw, 1);
+    });
+
+    it('instance with values and derivative values populated >> update >> should decrease values by with discounted derivatives', () => {
+      sut.update(0.1);
+
+      expectValuesToBe([-0.1, 0.9, 1.9, 2.9, 3.9, 4.9]);
+    });
+
+    it('instance with values and derivative values populated >> update >> should reset derivative values to zero', () => {
+      sut.update(0.1);
+
+      expectDerivativesToBe([0, 0, 0, 0, 0, 0]);
+    });
+
+    const expectValuesToBe = (expected: Array<number>) => {
+      for (let i = 0; i < expected.length; i++) {
+        expect(sut.w[i]).toBe(expected[i]);
+      }
+    };
+
+    const expectDerivativesToBe = (expected: Array<number>) => {
+      for (let i = 0; i < expected.length; i++) {
+        expect(sut.dw[i]).toBe(expected[i]);
+      }
+    };
+  });
+
+  describe('JSON:', () => {
+
+    const sut = Mat;
+    
+    describe('fromJSON:', () => {
+
+      let actual: Mat;
+
+      it('json object >> fromJSON >> should return a matrix with given dimensions', () => {
+        const json = { rows: 2, cols: 3, w: [0, 1, 2, 3, 4, 5] };
+        actual = sut.fromJSON(json);
+  
+        expect(actual.rows).toBe(2);
+        expect(actual.cols).toBe(3);
+      });
+  
+      it('json object >> fromJSON >> should return a matrix with values populated', () => {
+        const json = { rows: 2, cols: 3, w: [0, 1, 2, 3, 4, 5] };
+        actual = sut.fromJSON(json);
+  
+        expectValuesToBe([0, 1, 2, 3, 4, 5]);
+      });
+  
+      it('json object >> fromJSON >> should return a matrix with derivatives populated with zero', () => {
+        const json = { rows: 2, cols: 3, w: [0, 1, 2, 3, 4, 5] };
+        actual = sut.fromJSON(json);
+  
+        expectDerivativesToBe([0, 0, 0, 0, 0, 0]);
+      });
+
+      const expectValuesToBe = (expected: Array<number>) => {
+        for (let i = 0; i < expected.length; i++) {
+          expect(actual.w[i]).toBe(expected[i]);
+        }
+      };
+
+      const expectDerivativesToBe = (expected: Array<number>) => {
+        for (let i = 0; i < expected.length; i++) {
+          expect(actual.dw[i]).toBe(expected[i]);
+        }
+      };
+    });
+
+    describe('toJSON:', () => {
+
+      let m: Mat;
+      let actual: any;
+
+      beforeEach(() => {
+        m = new Mat(2, 3);
+        m.setFrom([0, 1, 2, 2, 1, 0]);
+      });
+
+      it('matrix populated with [0, 1, 2, 2, 1, 0] >> toJSON >> should return a json object with given rows and cols', () => {
+        actual = sut.toJSON(m);
+
+        expect(actual.rows).toBe(2);
+        expect(actual.cols).toBe(3);
+      });
+
+      it('matrix populated with [0, 1, 2, 2, 1, 0] >> toJSON >> should return a json object with values [w]', () => {
+        actual = sut.toJSON(m);
+
+        expectValuesToBe([0, 1, 2, 2, 1, 0]);
+      });
+
+      const expectValuesToBe = (expected: Array<number>) => {
+        for(let i = 0; i < expected.length; i++) {
+          expect(actual.w[i]).toBe(expected[i]);
+        }
+      };
+    });
   });
 });
